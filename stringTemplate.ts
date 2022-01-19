f9cefb285d6326872e69290f653fb7f72731aac7
mamidipallysoujanya <mamidipallysoujanya93@gmail.com>
2022-01-19 11:28:45 +0530

    string template

---
+++ stringTemplate.ts
@@ -0,0 +1,16 @@
+let num1=9;
+let num2=6;
+let result=num1+num2;
+//console.log("the addition of num1 and num2=result");
+
+//console.log(`the addition of num1 and num2 is = result`);
+console.log(`the addition of ${num1} and ${num2} is ${result}`);
+
+
+const greeting:string="Hello";
+const sname:string="Typescript";
+const templateString:string=`${greeting}
+...
+...
+welcome to ${sname}`;
+console.log(templateString);
